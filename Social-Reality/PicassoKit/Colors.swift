d4e71df9494a2e51a564a2e4c9cff9b1d5accb8f
ncrews35 <nickcrews35@gmail.com>
2021-07-12 00:11:57 -0400

    Updated SwiftUI Work

---
+++ Social-Reality/PicassoKit/Colors.swift
@@ -0,0 +1,19 @@
+//
+//  Colors.swift
+//  Social-Reality
+//
+//  Created by Nick Crews on 7/11/21.
+//
+
+import SwiftUI
+
+extension Color {
+
+    static let background = Color("Background")
+    static let primary = Color("Primary")
+    static let mainText = Color("MainText")
+    static let grayText = Color("GrayText")
+    static let tinted = Color("Tinted")
+    static let separator = Color("Separator")
+
+}
